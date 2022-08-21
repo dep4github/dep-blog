234cc6e2a63ff8c87f020a21f279cc92bdc42525
Anggia Ramadhan <anggia@pintarnya.com>
2022-08-21 17:15:51 +0700

    add prettier and eslint

--- app/root.tsx
+++ app/root.tsx
@@ -1,22 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'Anggia Riksa Ramadhan',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
@@ -28,5 +21,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
